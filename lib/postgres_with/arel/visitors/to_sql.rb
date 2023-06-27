f79198cc79e25ab4d4e5350d2398c39b8792e5f4
Derrick Neier <derrick.neier@gmail.com>
2023-06-27 15:43:06 -0400

    fixing materialized

--- lib/postgres_with/arel/visitors/to_sql.rb
+++ lib/postgres_with/arel/visitors/to_sql.rb
@@ -6,6 +6,31 @@ module Arel
         collector << " AS MATERIALIZED "
         visit o.right, collector
       end
+
+      def collect_ctes(children, collector)
+        children.each_with_index do |child, i|
+          collector << ", " unless i == 0
+
+          case child
+          when Arel::Nodes::As, Arel::Nodes::AsMaterialized
+            name = child.left.name
+            relation = child.right
+          when Arel::Nodes::TableAlias
+            name = child.name
+            relation = child.relation
+          end
+
+          collector << quote_table_name(name)
+          if child.class == Arel::Nodes::AsMaterialized
+            collector <<  " AS MATERIALIZED "
+          else
+            collector <<  " AS "
+          end
+          visit relation, collector
+        end
+
+        collector
+      end
     end
   end
 end
